7f4dc2845dd9f75748a44684a8a9a5fbadbd68e6
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2023-10-13 17:40:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import com.google.common.primitives.Ints;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Ints.compare(5, 3));
+
+        System.out.println(Ints.compare(2, 2));
+    }
+}
